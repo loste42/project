af7741529d0bfbb8c57bba3b21aaff5fcec173c2
mlhoste <michaellhoste@web.de>
2015-08-10 18:26:33 +0000

    Paperclip for movie images

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -74,6 +74,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:title, :description, :year, :picture)
+      params.require(:movie).permit(:title, :description, :year, :picture, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806160110) do
+ActiveRecord::Schema.define(version: 20150810175024) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(version: 20150806160110) do
     t.integer  "year"
     t.string   "picture"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "movies", ["user_id"], name: "index_movies_on_user_id"
