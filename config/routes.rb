fa2cd11c24fb32033e6f1a479ef6b4bc0b14e0d4
mlhoste <michaellhoste@web.de>
2015-08-11 18:47:35 +0000

    New MovieIndex/Show

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   resources :movies
   devise_for :users
   #get 'users/new'
+  
+
 
   root 'static_pages#home'
+
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contacts' => 'static_pages#contacts'
