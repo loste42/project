61845da59ab73837ae65e61756ce3466c350ef11
mlhoste <michaellhoste@web.de>
2015-08-13 18:21:59 +0000

    starsstarsstars

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,12 @@ class MoviesController < ApplicationController
   # GET /movies/1.json
   def show
       @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+      
+      if @review.blank?
+        @avg_review = 2
+      else
+        @avg_review = @reviews.average(:rating).round(2)
+      end
   end
 
   # GET /movies/new
